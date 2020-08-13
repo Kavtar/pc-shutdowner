06dde7e6200b2963d821b462c33972d54a1f1166
zKavtaradze <ZurabKavtaradze@protonmail.com>
2020-08-13 14:35:02 +0400

    initial create

---
+++ ShutDowner/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Topshelf;
+
+namespace ShutDowner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(cfg => 
+            {
+                cfg.Service<Service>(ins => 
+                {
+                    ins.ConstructUsing(() => new Service());
+                    ins.WhenStarted(a => a.Start());
+                    ins.WhenStopped(a => a.Stop());
+                });
+
+                cfg.SetServiceName("ShutDowner");
+                cfg.SetServiceName("Scheduled PC Shutdowner Service");
+                cfg.SetDescription("The service shuts down computer automatically at midnight;" +
+                                    "Is useful for people who are lazy enough to do it manually.");
+
+                cfg.StartAutomatically();
+            });
+        }
+    }
+}
---
+++ ShutDowner/Service.cs
@@ -0,0 +1,18 @@
+﻿using static ShutDowner.Jobs;
+
+namespace ShutDowner
+{
+    internal class Service
+    {
+        public bool Start()
+        {
+            ShutDown();
+            return true;
+        }
+
+        public bool Stop()
+        {
+            return true;
+        }
+    }
+}
